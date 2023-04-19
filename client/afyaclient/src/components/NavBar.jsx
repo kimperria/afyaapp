51af967ad6fc3f08ed41a39170f41e5aeea2b390
John-Kimani <kimwaweru100@gmail.com>
2023-04-19 11:58:54 +0300

    feat: Create a navbar component

---
+++ client/afyaclient/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">Karibu, Afya APP</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mx-auto">
+          <Nav.Link href="/register-patient">Register Patient</Nav.Link>
+          <Nav.Link href="/reports">Patient Records</Nav.Link>
+        </Nav>
+
+        <Nav className="my-auto">
+          {/* <Nav.Link href="/register-patient">Admin</Nav.Link> */}
+          <Nav.Link href="#logout">Logout</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default NavBar

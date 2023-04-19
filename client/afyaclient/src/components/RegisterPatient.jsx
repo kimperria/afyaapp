8db9feb75cc0465dfa422c5962f885cb9bfe279e
John-Kimani <kimwaweru100@gmail.com>
2023-04-19 12:11:42 +0300

    feat: insert gender option dropdown

--- client/afyaclient/src/components/RegisterPatient.jsx
+++ client/afyaclient/src/components/RegisterPatient.jsx
@@ -14,30 +14,39 @@ function RegisterPatient() {
         </Alert>
         <div className="mx-auto col-10 col-md-8 col-lg-6">
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3" controlId="formBasicFirstName">
               <Form.Label>First Name</Form.Label>
               <Form.Control type="text" placeholder="First name" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3" controlId="formBasicLastName">
               <Form.Label>Last Name</Form.Label>
               <Form.Control type="text" placeholder="last name" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3" controlId="formBasicDOB">
               <Form.Label>Date of birth</Form.Label>
               <Form.Control type="date" placeholder="Enter your DOB" />
             </Form.Group>
 
+            <Form.Group className="mb-3" controlId="formBasicGender">
+              <Form.Select aria-label="Default select example">
+                <option>Please select your gender</option>
+                <option value="1">Male</option>
+                <option value="2">Female</option>
+                <option value="3">Non-Binary</option>
+              </Form.Select>
+            </Form.Group>
+
             <section className="row">
               <div className="col">
-                <Button variant="danger" type="submit" style={{width:"100%"}}>
+                <Button variant="danger" type="submit" style={{ width: "100%" }}>
                   Clear
                 </Button>
               </div>
 
               <div className="col">
-                <Button variant="success" type="submit" style={{width:"100%"}}>
+                <Button variant="success" type="submit" style={{ width: "100%" }}>
                   Save
                 </Button>
               </div>

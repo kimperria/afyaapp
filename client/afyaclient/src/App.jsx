a5ee1996a42dfacdaf704b6d1fe2650df04f2d31
John-Kimani <kimwaweru100@gmail.com>
2023-04-19 10:54:59 +0300

    feat: update folder structure

--- client/afyaclient/src/App.jsx
+++ client/afyaclient/src/App.jsx
@@ -5,7 +5,7 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 import Login from "./screens/auth/Login";
-import RegisterPatient from "./screens/RegisterPatient";
+import RegisterPatient from "./components/RegisterPatient";
 
 function App() {
   return (

694226510a7ae86d84b40e9df7d3cdfb2a8ecf95
John-Kimani <kimwaweru100@gmail.com>
2023-04-20 16:24:47 +0300

    bug: keyError accessing username

--- server/afyaapp/serializers.py
+++ server/afyaapp/serializers.py
@@ -7,19 +7,20 @@ from rest_framework.exceptions import AuthenticationFailed
 
 
 class LoginSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(max_length=30, min_length=5)
+    username = serializers.CharField(max_length=30, min_length=5)
     password = serializers.CharField(max_length=68, min_length=6, write_only=True)
     
 
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ('username', 'password')
 
     def validate(self, attrs):
-        email = attrs.get('email', '')
+        username = attrs.get('username', '')
         password = attrs.get('password', '')
 
-        authenticated_user = auth.authenticate(email=email, password=password)
+        authenticated_user = auth.authenticate(username=username, password=password)
+        print(authenticated_user, password)
 
         if not authenticated_user:
             raise AuthenticationFailed('Invalid credentials, please try again')
@@ -28,5 +29,6 @@ class LoginSerializer(serializers.ModelSerializer):
             raise AuthenticationFailed('Disabled account, please contact admin')
         
         return {
-            'email': authenticated_user.email,
+            'email': authenticated_user.username,
+            # 'token': authenticated_user.token
         }

db8a1ee4b0cece4f8adccc565fef61c4bc29813f
John-Kimani <kimwaweru100@gmail.com>
2023-04-20 18:40:53 +0300

    feat: build endpoints to update and create patient instance

--- server/afyaapp/views.py
+++ server/afyaapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Profile
-from .serializers import LoginSerializer
+from .serializers import LoginSerializer, PatientInformationSerializer
 
 from rest_framework import generics, status
 from rest_framework.response import Response
@@ -37,3 +37,32 @@ class LoginAPIView(generics.GenericAPIView):
             }
 
             return Response(data=response, status=status.HTTP_200_OK)
+        
+class PatientInformationView(generics.GenericAPIView):
+
+    serializer_class = PatientInformationSerializer
+
+    @swagger_auto_schema(operation_summary='Save patient information')
+    def post(self, request):
+
+        patient = request.data
+
+        serializer = self.serializer_class(data=patient)
+
+        if serializer.is_valid(raise_exception=True):
+
+            response = {
+                'success-status': True, 
+                'patient-information': serializer.data
+            }
+
+        return Response(data=response, status=status.HTTP_200_OK)
+    
+    @swagger_auto_schema(operation_summary='Get patient information')
+    def get(self, request):
+        pass
+    
+    @swagger_auto_schema(operation_summary='Update patient information')
+    def put(self, request):
+        pass
+

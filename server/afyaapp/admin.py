fd639f2c9bfa891af337e783d97d3a534433f5e1
John-Kimani <kimwaweru100@gmail.com>
2023-04-20 15:36:23 +0300

    feat: register and configure additional models build patientInformation, AppointmentDetails and patient record models

--- server/afyaapp/admin.py
+++ server/afyaapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, PatientInformation, AppointmentDetails, PatientRecord
 
 admin.site.register(Profile)
+admin.site.register(PatientInformation)
+admin.site.register(AppointmentDetails)
+admin.site.register(PatientRecord)
--- server/afyaapp/models.py
+++ server/afyaapp/models.py
@@ -1,8 +1,56 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.user.username
+
+class PatientInformation(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(blank=True, null=True)
+    registered_on = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f'Fullname: {self.first_name} ' + self.last_name
+
+class AppointmentDetails(models.Model):
+    patient = models.OneToOneField(PatientInformation, on_delete=models.CASCADE)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    body_mass_index = models.IntegerField()
+
+    def __str__(self):
+        return self.patient.first_name
+
+class PatientRecord(models.Model):
+
+    GOOD = 1
+    POOR = 2
+    UNDEFINED = 0
+
+    PATIENT_CHOICE = (
+        (GOOD, 'good'),
+        (POOR, 'poor'),
+        (UNDEFINED, 'undefined')
+    )
+
+    YES = 1
+    NO = 2
+    DOUBTFUL = 0
+
+    PATIENT_ANSWER = (
+        (YES, 'yes'),
+        (NO, 'no'),
+        (DOUBTFUL, 'doubtful')
+    )
+
+    general_health = models.IntegerField(choices=PATIENT_CHOICE, default=UNDEFINED)
+    is_onDiet = models.IntegerField(choices=PATIENT_ANSWER, default=PATIENT_ANSWER, null=True, blank=True)
+    is_onDrugs = models.IntegerField(choices=PATIENT_ANSWER, default=PATIENT_ANSWER, null=True, blank=True)
+
+    def __str__(self):
+        return self.general_health

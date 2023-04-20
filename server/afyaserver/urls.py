eb878b854396e5b97fe6315b4367e04c5ed79cf7
John-Kimani <kimwaweru100@gmail.com>
2023-04-20 16:05:53 +0300

    feat: configure simple JWT

--- server/afyaserver/urls.py
+++ server/afyaserver/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -34,6 +38,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    ##JWT Auth routes
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    ## app routes
     path('admin/', admin.site.urls),
     path('api/', include('afyaapp.urls')),
 

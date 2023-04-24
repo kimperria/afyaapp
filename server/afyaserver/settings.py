68ce40f5be079fe0c45d3d9c7661e951b8f745c1
John-Kimani <kimwaweru100@gmail.com>
2023-04-24 15:05:00 +0300

    bug: cors errors

--- server/afyaserver/settings.py
+++ server/afyaserver/settings.py
@@ -84,6 +84,31 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 CSRF_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_ALLOW_ALL = True

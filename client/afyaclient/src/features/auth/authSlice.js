86a32d5915cb3e4aa65be085d155dc033b81c8e7
John-Kimani <kimwaweru100@gmail.com>
2023-05-03 13:57:47 +0300

    feat: redirect user on login

--- client/afyaclient/src/features/auth/authSlice.js
+++ client/afyaclient/src/features/auth/authSlice.js
@@ -11,7 +11,8 @@ const authSlice = createSlice({
     name: 'auth',
     initialState: {
         accessToken:accessToken.access,
-        refreshToken: accessToken.refresh
+        refreshToken: accessToken.refresh,
+        isAuthenticated: false
     },
     reducers: {
         // action creators
@@ -21,12 +22,16 @@ const authSlice = createSlice({
         setRefreshToken: (state, action) => {
             state.refreshToken = action.payload
         },
+        setIsAuthenticated: (state, action) => {
+            state.isAuthenticated = action.payload
+        },
         logout: (state, action) => {
             state.accessToken = null,
-            state.refreshToken = null
+            state.refreshToken = null,
+            state.isAuthenticated = false
         }
     } 
 });
 
-export const { setAccessToken, setRefreshToken, logout } = authSlice.actions; //access individual action creator func
+export const { setAccessToken, setRefreshToken, setIsAuthenticated, logout } = authSlice.actions; //access individual action creator func
 export default authSlice.reducer; //for store config

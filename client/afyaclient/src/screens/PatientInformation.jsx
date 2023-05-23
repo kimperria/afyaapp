17a8fe27477749b63651ccf67de7838ff909ab63
John-Kimani <kimwaweru100@gmail.com>
2023-05-23 15:29:04 +0300

    feat: patially display patient information

--- client/afyaclient/src/screens/PatientInformation.jsx
+++ client/afyaclient/src/screens/PatientInformation.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import VitalSection from "../components/VitalSection";
 import Card from "react-bootstrap/Card";
 import { useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+
+import { useViewPatientIDQuery } from "../features/patients/patientAPISlice";
 function PatientInformation() {
   const { id } = useParams();
+
+  const [patient, setPatient ] = useState({});
+
+  const { data, isLoading, error } = useViewPatientIDQuery(id);
+
+  useEffect(() => {
+    if(data != null){
+      setPatient(data)
+      console.log(patient)
+    }
+  }, [data])
 
   return (
     <main className="container container-fluid">
@@ -24,8 +37,9 @@ function PatientInformation() {
             <Card.Body>
               <div className="row">
                 <div className="col-md-6">
-                  <h5>Full Name: </h5>
-                  <h5>Age: </h5>
+                  <h5>Full Name: {patient.first_name + ' ' + patient.last_name}</h5>
+                  <h5>Age: {patient.age}</h5>
+                  <h5>Date of Birth: {patient.date_of_birth}</h5>
 
                   <h5>BMI Status: </h5>
                 </div>
